7424b5183c174c115d444095513fe458cc0514c9
KING <abayemiracle@gmail.com>
2021-01-08 23:31:52 -0800

    serverpress is coming live

--- server.js
+++ server.js
@@ -14,10 +14,17 @@ const port = process.env.PORT || 5288;
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/views'));
+app.set('view engine', 'pug');
 
-app.get('/',cors, (req,res) => {
-   res.sendFile('index');
-});
+// app.get('/',cors, (req,res) => {
+//    res.sendFile('index');
+// });
+
+
+app.get('/', (req,res)=>{
+    res.render(__dirname + `/views/index`);
+ });
+
 
 
 
